234f8b304f43cdce902478cc845d32add130e385
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2023-07-20 16:42:49 +0200

    navbar

--- task/urls.py
+++ task/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path('', views.task_list, name='task_list'),
+    path('calendar/', views.calendar_view, name='calendar'),
+    path('get_tasks_json/', views.get_tasks_json, name='get_tasks_json'),
+    path('create/', views.task_create, name='task_create'),
+    path('<int:pk>/', views.task_detail, name='task_detail'),
+    path('<int:pk>/update/', views.task_update, name='task_update'),
+    path('<int:pk>/delete/', views.task_delete, name='task_delete'),
 ]
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
-from task import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('task/', views.task_list, name='task_list'),
-    path('task/calendar/', views.calendar_view, name='task_calendar'),
-    path('task/get_tasks_json/', views.get_tasks_json, name='get_tasks_json'),
-    path('task/create/', views.task_create, name='task_create'),
-    path('task/<int:pk>/', views.task_detail, name='task_detail'),
-    path('task/<int:pk>/update/', views.task_update, name='task_update'),
-    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
+    path('admin/', admin.site.urls),
+    path('task/', include('task.urls')),
 ]
